3c3a894780fc347fa065d125b791bb88705db478
Nimmi gupta <nimmi.gupta@gmail.com>
2022-09-09 17:44:23 -0400

    v1

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,12 +2,19 @@
 
 echo "I am in frontend !!"
 
+#Validate if any script fail don't go to the next line.
+set -e
+
 source components/common.sh
 
+echo "Installing Nginx :"
 yum install nginx -y >> /tmp/frontend.log
 systemctl enable nginx
+
+echo "Starting Nginx :"
 systemctl start nginx
 
+echo "Download the frontend.zip to the /tmp"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
