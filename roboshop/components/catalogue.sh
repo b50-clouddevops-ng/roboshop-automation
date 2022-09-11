660a1b70cf5885ac49dd17615946ddf6faf02b9a
Nimmi gupta <nimmi.gupta@gmail.com>
2022-09-11 13:04:20 -0400

    Catalogue setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -7,13 +7,30 @@ set -e
 
 source components/common.sh
 
+echo -n "Configure yum repos for nodejs :"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+stat $?
 
 echo -n "Install Nodejs :"
 yum install nodejs -y >> /tmp/nodejs.log
 stat $?
 
-echo "Create a user call roboshot :"
+echo -n "adding a user call roboshot :"
 useradd roboshop
 stat $?
 
+echo -n "Downloading the components :"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip" >> /tmp/nodejs.log
+stat $?
+
+echo -n "Cleanup of old cataloge contents :"
+#Code to be add
+
+echo -n "Extracting cataloge contents :"
+cd /home/roboshop && unzip /tmp/catalogue.zip && mv catalogue-main catalogue
+stat $?
+
+echo -n "Install the nodeJS :"
+cd /home/roboshop/catalogue
+npm install
+stat $?
