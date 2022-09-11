077ef08f96813268812b33e40c89aec73ef5cca6
Nimmi gupta <nimmi.gupta@gmail.com>
2022-09-11 11:08:57 -0400

    frontend changes

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -21,13 +21,23 @@ echo -n "Download the schema :"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 stat $?
 
-cd /usr/share/nginx/html
-rm -rf *
+cd /usr/share/nginx/html && rm -rf *
+
+echo -n "Extract the script"
 unzip -o /tmp/frontend.zip >> /tmp/frontend.log
-mv frontend-main/* .
-mv static/* .
+stat $?
+
+echo -n "Move the script to ngnix"
+mv frontend-main/* . && mv static/* . 
+stat $?
+
+echo -n "Remove the unnessary file from /tmp file"
 rm -rf frontend-main README.md
+stat $?
+
+echo -n "Cofiguring the Reverse Proxy"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $?
 
 echo -n "Restarting Ngnix :"
 systemctl restart nginx
