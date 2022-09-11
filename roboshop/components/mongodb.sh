a5e5028039760626a3cf26e944248914e61b5c01
Nimmi gupta <nimmi.gupta@gmail.com>
2022-09-11 11:45:55 -0400

    MongoDB setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 echo "I am in MongoDb"
 
-#Validate if any script fail don't go to the next line.
+#Validate if any script fail, don't go to the next line.
 set -e
 
 #Call the common functions
 source components/common.sh
 
-echo "Setup MongoDB repos"
+echo -n "1. Setup MongoDB repos"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
 stat $?
+
+echo -n "2. Install Mongo & Start Service."
+yum install -y mongodb-org
+stat $?
+
+systemctl enable mongod
+echo -n " start mongodb "
+systemctl start mongod
+
+
