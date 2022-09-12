63c91a525d6fbb65b52e0ac6cdde7653f46cc2ef
Nimmi gupta <nimmi.gupta@gmail.com>
2022-09-11 20:46:34 -0400

    Redisgsetup

--- roboshop/components/redish.sh
+++ roboshop/components/redish.sh
@@ -9,10 +9,23 @@ source components/common.sh
 
 
 echo  -n "Get the repos"
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo >> /tmp/redish.log
 stat $?
 
 echo  -n "Install redish"
-yum install redis-6.2.7 -y
+yum install redis-6.2.7 -y >> /tmp/redish.log
 stat $?
+
+
+sed -i -e 's/127.0.0/0.0.0.0' /etc/redis.conf'
+
+echo  -n "Enable redish : "
+systemctl enable redis
+stat $?
+
+echo  -n "Start redish : "
+systemctl start redis
+stat $?
+
+systemctl status redis -l
 
